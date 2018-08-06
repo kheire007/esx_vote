fbf07f607437a26b80ea4fdb2b21c402d56f609b
kheire007 <36076014+kheire007@users.noreply.github.com>
2018-08-06 23:53:31 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,8 @@
+client_script {
+	"config.lua",
+	"client.lua"
+}
+server_script {
+	"@mysql-async/lib/MySQL.lua",
+	"server.lua"	
+}
---
+++ config.lua
@@ -0,0 +1,12 @@
+Config                           = {}
+Config.DrawDistance              = 100.0
+Config.MarkerColor               = {r = 39, g = 131, b = 206}
+
+Config.Zones = {
+
+	VoteBooth1 = {
+		MarkerPos  = {x = -266.36416625977, y = -2035.6640625, z = 29.145593643188 },
+		Size       = {x = 1.5, y = 1.5, z = 1.0},
+		Type       = 1
+	}
+}
